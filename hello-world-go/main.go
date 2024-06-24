a6348b023309fda54457a78575c830f2455953bc
unknown <grosclaude.daniela@gmail.com>
2024-06-24 01:16:07 -0300

    initial commit

---
+++ hello-world-go/main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"hello-world-go/doctor"
+	"os"
+	"strings"
+)
+
+func main() {
+	reader := bufio.NewReader(os.Stdin)
+	whatToSay := doctor.Intro()
+
+	fmt.Println(whatToSay)
+
+	for {
+		fmt.Print("->")
+		userInput, _ := reader.ReadString('\n')
+		userInput = strings.Replace(userInput, "\r\n", "", -1)
+		userInput = strings.Replace(userInput, "\n", "", -1)
+
+		if userInput == "quit" {
+			break
+		} else {
+			fmt.Println(doctor.Response(userInput))
+		}
+	}
+}
